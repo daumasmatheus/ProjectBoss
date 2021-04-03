98660e4d1d8b196ca3cd7ae1da7905a0c27b36cd
matheus daumas <daumasmatheussouza@gmail.com>
2021-04-03 14:32:09 -0300

    Validação do email e senha no formulario de login no frontend

--- src/ProjectManager-client/src/app/components/account/log-in/log-in.component.ts
+++ src/ProjectManager-client/src/app/components/account/log-in/log-in.component.ts
@@ -31,8 +31,8 @@ export class LogInComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
     }); 
   }
 
