64a6b19d1e1a2493b232a7b1f6fcbffbf171b3e5
matheus daumas <daumasmatheussouza@gmail.com>
2021-04-04 20:03:07 -0300

    Ajuste para permitir apenas a mudanca de status de tarefas no board em que o usuário é responsavel

--- src/ProjectManager-client/src/app/components/dashboard/projects/board/board.component.ts
+++ src/ProjectManager-client/src/app/components/dashboard/projects/board/board.component.ts
@@ -65,8 +65,8 @@ export class BoardComponent implements OnInit {
   }
 
   toggleTaskStatus(event: CdkDragDrop<TaskModel[]>) {
-    if (event.item.data.authorId == this.localStorageUtils.getUser().id || 
-        (this.localStorageUtils.checkUserClaim("Administrator") || this.localStorageUtils.checkUserClaim("Project Manager"))){
+    if (event.item.data.attendantId == this.localStorageUtils.getUser().id || 
+       (this.localStorageUtils.checkUserClaim("Administrator") || this.localStorageUtils.checkUserClaim("Project Manager"))){
       if (event.previousContainer === event.container) {
         moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
       } else {      
