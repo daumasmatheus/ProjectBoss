0f19c29c28772a061159f3a2dba7a8a62802b145
matheus daumas <daumasmatheussouza@gmail.com>
2021-04-10 17:58:11 -0300

    Incluido os endereços de produção

--- src/ProjectManager-client/src/environments/environment.prod.ts
+++ src/ProjectManager-client/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://api.projectboss.xyz/',
+  linkedin: 'https://www.linkedin.com/in/matheus-daumas-800003109/',
+  github: 'https://github.com/daumasmatheus'
 };
